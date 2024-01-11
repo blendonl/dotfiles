9c895eb831a9c98230359c1743001e2b0670e334
blendonl <blendonluta@gmail.com>
2024-01-11 19:56:19 +0100

    update nvim

---
+++ nvim/lua/plugins/editor/markdown.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		'ixru/nvim-markdown',
+		setup = {}
+	},
+	-- {
+	-- 	"nvim-neorg/neorg",
+	-- 	run = ":Neorg sync-parsers", -- This is the important bit!
+	-- 	opts = {},
+	-- 	-- config = function()
+	-- 	-- 	require("neorg").setup {
+	-- 	-- 		-- configuration here
+	-- 	-- 	}
+	-- 	-- end,
+	-- },
+	{
+		'jghauser/follow-md-links.nvim',
+		setup = {}
+	}
+}
