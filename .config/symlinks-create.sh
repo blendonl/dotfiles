5b3967214d4c0ec5aeded4fc2273a056709d2e4a
blendonl <blendonluta@gmail.com>
2025-05-12 15:52:54 +0200

    feat: idk

---
+++ .config/symlinks-create.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.config/hypr ~/.config/hypr
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/dunst ~/.config/dunst
+ln -s ~/dotfiles/.config/sherlock ~/.config/sherlock
