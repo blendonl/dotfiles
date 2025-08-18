274a1661e15cc73ed6a8057dd0a8ead5ff33e3f4
blendonl <blendonluta@gmail.com>
2025-08-18 06:57:35 +0200

    change scripts

--- .config/tmux/scripts/claude.sh
+++ .config/tmux/scripts/claude.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-selected_name=$(tmux display-message -p '#S')
-selected_path=$(tmux display-message -p '#{pane_current_path}')
+WINDOW_LIST=$(tmux list-windows)
+CLAUDE_WINDOW="$(tmux list-windows | grep  claude | egrep -o '^[^:]+' )"
+ACTIVE_WINDOW="$(tmux list-windows | grep  "*" | egrep -o '^[^:]+' )"
+
+LIST_PANES="$(tmux list-panes -F '#F' )"
+PANE_COUNT="$(echo "${LIST_PANES}" | wc -l | bc)"
+
+if [ "${PANE_COUNT}" = 1 ]; then
+    if [ -z "${CLAUDE_WINDOW}" ]; then
+        tmux new-window -d -n claude -c "#{pane_current_path}" claude 
+
+        CLAUDE_WINDOW="$(tmux list-windows | grep  claude | egrep -o '^[^:]+' )"
+    fi
+
+    tmux join-pane -t $ACTIVE_WINDOW -s $CLAUDE_WINDOW
+else
+    tmux break-pane -d
+fi
 
 
-tmux display-popup -t "$selected_name" -d "$selected_path" -xC -yC -w80% -h80% -E "claude"
 
 
