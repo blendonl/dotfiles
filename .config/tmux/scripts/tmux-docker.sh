5d94b477f49a452198ea64b8b0623ca8a128746d
blendonl <blendonluta@gmail.com>
2025-09-11 10:36:52 +0200

    fix something

--- .config/tmux/scripts/tmux-docker.sh
+++ .config/tmux/scripts/tmux-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 WINDOW_LIST=$(tmux list-windows)
-RUN_WINDOW="$(tmux list-windows | grep  run | egrep -o '^[^:]+' )"
+DOCKER_WINDOW="$(tmux list-windows | grep  docker | egrep -o '^[^:]+' )"
 ACTIVE_WINDOW="$(tmux list-windows | grep  "*" | egrep -o '^[^:]+' )"
 
 LIST_PANES="$(tmux list-panes -F '#F' )"
@@ -10,21 +10,20 @@ PANE_COUNT="$(echo "${LIST_PANES}" | wc -l | bc)"
 
 
 
-if [ "${PANE_COUNT}" -gt 1 ] && [ -z "${RUN_WINDOW}" ]; then
+if [ "${PANE_COUNT}" -gt 1 ] && [ -z "${DOCKER_WINDOW}" ]; then
     
-    tmux break-pane -d -n run
-
+    tmux break-pane -s $ACTIVE_WINDOW.1 -d -n run
 
     exit 0
 fi
 
-if [ -z "${RUN_WINDOW}" ]; then
-    tmux new-window -d -n run -c "#{pane_current_path}" 
+if [ -z "${DOCKER_WINDOW}" ]; then
+    tmux new-window -d -n docker -c "#{pane_current_path}" 
 
-    RUN_WINDOW="$(tmux list-windows | grep  run | egrep -o '^[^:]+' )"
+    DOCKER_WINDOW="$(tmux list-windows | grep  docker | egrep -o '^[^:]+' )"
 fi
 
-tmux join-pane -h -l 25% -t $ACTIVE_WINDOW -s $RUN_WINDOW
+tmux join-pane -h -l 25% -t $ACTIVE_WINDOW -s $DOCKER_WINDOW
 
 
 
---
+++ .config/tmux/scripts/tmux-run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+WINDOW_LIST=$(tmux list-windows)
+RUN_WINDOW="$(tmux list-windows | grep  run | egrep -o '^[^:]+' )"
+ACTIVE_WINDOW="$(tmux list-windows | grep  "*" | egrep -o '^[^:]+' )"
+
+LIST_PANES="$(tmux list-panes -F '#F' )"
+PANE_COUNT="$(echo "${LIST_PANES}" | wc -l | bc)"
+
+
+
+
+if [ "${PANE_COUNT}" -gt 1 ] && [ -z "${RUN_WINDOW}" ]; then
+    
+    tmux break-pane -s $ACTIVE_WINDOW.1 -d -n run
+
+fi
+
+if [ -z "${RUN_WINDOW}" ]; then
+    tmux new-window -d -n run -c "#{pane_current_path}" 
+
+    RUN_WINDOW="$(tmux list-windows | grep  run | egrep -o '^[^:]+' )"
+fi
+
+tmux join-pane -h -l 25% -t $ACTIVE_WINDOW -s $RUN_WINDOW
+
+
+
+
