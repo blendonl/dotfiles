18943ce3b3e5d3bd0644c637f6451ec83b025fd7
blendonl <blendonluta@gmail.com>
2025-11-04 15:37:40 +0100

    add cliiphist

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 yay -S --needed  hyprland-git hyprcursor-git hyprgraphics-git hyprlang-git hyprlock-git hyprutils-git 
 yay -S --needed  xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-desktop-protal-hyprland
 yay -S --needed  noto-fonts-emoji ttf-firacode-nerd ttf-gabarito-git ttf-jetbrains-mono-nerd ttf-material-symbols-variable-git ttf-nerd-fonts-symbols ttf-sourcecodepro-nerd 
-yay -S --needed  tmux zsh jq eww stow wl-paste wl-copy dunst udiskie
+yay -S --needed  tmux zsh jq eww stow wl-clipboard dunst udiskie cliphist
 yay -S --needed  fzf npm yarn-berry cargo rustc luarocks lua51
 yay -S --needed  alacritty neovim-git sherlock-launcher-bin qutebrowser
 yay -S --needed  github-cli lazygit slack-desktop vencord posting 
