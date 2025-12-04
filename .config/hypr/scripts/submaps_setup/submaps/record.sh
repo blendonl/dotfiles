388a375131484fe45284a93e3827317fb6226a8e
blendonl <blendonluta@gmail.com>
2025-12-04 03:22:58 +0100

    something

--- .config/hypr/scripts/submaps_setup/submaps/record.sh
+++ .config/hypr/scripts/submaps_setup/submaps/record.sh
@@ -5,7 +5,7 @@ source ~/.config/hypr/scripts/submaps_setup/submap.sh record
 
 
 add_bind "v" "exec, hyprshot -m region --clipboard-only" "Screenshot region to clipboard"
-add_bind "s" "exec, grim -g \"\$(slurp -d)\" - | wl-copy" "Screenshot region to clipboard"
+add_bind "s" "exec, hyprshot -m region" "Screenshot region to clipboard"
 add_bind "p" "exec, hyprpicker -a" "Screenshot with picker"    
 
 
