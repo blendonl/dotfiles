83d6a268fe7607f4d5a9410fef5c12271013937b
blendonl <blendonluta@gmail.com>
2026-02-03 16:40:55 +0100

    update tmux: use backtick prefix, add claude personal mode, fix xargs -r

--- .config/tmux/scripts/modes/mode/ai.sh
+++ .config/tmux/scripts/modes/mode/ai.sh
@@ -3,10 +3,13 @@
 source $HOME/.config/tmux/scripts/modes/mode.sh
 
 
+CLAUDE_PERSONAL='CLAUDE_CONFIG_DIR=~/personal claude'
+
 create_mode "ai-mode" "a"
 
 add_bind "o" 'run-shell "~/.config/tmux/scripts/window/create-window.sh OPENCODE opencode"' "Open OpenCode window"
 add_bind "c" 'run-shell "~/.config/tmux/scripts/window/create-window.sh CLAUDE claude"' "Open Claude window"
+add_bind "p" "run-shell '~/.config/tmux/scripts/window/create-window.sh CLAUDE_PERSONAL $CLAUDE_PERSONAL'" "Open Claude window"
 add_bind "x" 'run-shell "~/.config/tmux/scripts/window/create-window.sh CODEX codex"' "Open Codex window"
 
 
