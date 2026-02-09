2973d8ee6fe3599babc7bc342b12676d11a7ed22
blendonl <blendonluta@gmail.com>
2026-02-09 14:24:17 +0100

    fix reserved space calculation for active monitor

--- .config/hypr/scripts/window/reserved-space.sh
+++ .config/hypr/scripts/window/reserved-space.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 
-res_pos=500
+res_pos=600
+
+active_monitor=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .name')
 
 
 
-if grep -q "monitor = , addreserved, 0, 0, 0, $res_pos" ~/.config/hypr/hyprland/reserved-space.conf; then
+
+if grep -q "monitor = $active_monitor, addreserved, 0, 0, 0, $res_pos" ~/.config/hypr/hyprland/reserved-space.conf; then
     source ~/.config/hypr/scripts/window/remove-reserved-space.sh
 fi
 
 
-echo "monitor = , addreserved, 0, 0, 0, $res_pos" > ~/.config/hypr/hyprland/reserved-space.conf
+echo "monitor = $active_monitor, addreserved, 0, 0, 0, $res_pos" > ~/.config/hypr/hyprland/reserved-space.conf
 
 
 if ! grep -q "source = ~/.config/hypr/hyprland/reserved-space.conf" ~/.config/hypr/hyprland.conf; then
@@ -19,7 +22,6 @@ fi
 
 
 
-active_monitor=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .name')
 width=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .width')
 height=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .height')
 x=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .x')
@@ -27,11 +29,10 @@ y=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .y')
 scale=$(hyprctl -j monitors | jq -r '.[] | select(.focused==true) | .scale')
 
 
+actual_width=$(printf "%.0f" $(echo "$width / $scale" | bc -l))
+move=$(printf "%.0f" $(echo "$actual_width - $res_pos" | bc -l))
+res_pos=$(printf "%.0f" $(echo "$res_pos * 100 / $actual_width" | bc -l))
 
-
-
-move=$((x + ((width ) - res_pos)))
-res_pos=$((res_pos * 100 / (width)))
 
 
 
