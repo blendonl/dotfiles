dfa735c72cde3c0b0a1910fe7e7099e215ad3c77
blendonl <blendonluta@gmail.com>
2026-02-09 14:24:15 +0100

    update gaming keybinds to launch steam and kovaaks workspaces

--- .config/hypr/scripts/submaps_setup/submaps/gaming.sh
+++ .config/hypr/scripts/submaps_setup/submaps/gaming.sh
@@ -2,10 +2,8 @@
 
 source ~/.config/hypr/scripts/submaps_setup/submap.sh gaming
 
-add_bind "w" "submap, work" "Work"
-add_bind "p" "submap, personal" "Personal"
-add_bind "g" "submap, gaming" "Gaming"
-add_bind "c" "submap, content" "Content"
+add_bind "s" "focusworkspaceoncurrentmonitor, name:steam" "Steam"
+add_bind "k" "focusworkspaceoncurrentmonitor, name:kovaaks" "Kovaaks"
 
 
 
