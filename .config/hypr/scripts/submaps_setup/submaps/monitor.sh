9b829a2db577507b14d5653da76e0ccb23f261e1
blendonl <blendonluta@gmail.com>
2026-02-10 11:57:39 +0100

    add monitor submap for focus and workspace movement

---
+++ .config/hypr/scripts/submaps_setup/submaps/monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/.config/hypr/scripts/submaps_setup/submap.sh monitor
+
+
+
+add_bind "F" "focusmonitor, +1" "Focus Monitor"
+add_bind "M" "movecurrentworkspacetomonitor, +1" "Move current workspace to monitor"
+
+
+
