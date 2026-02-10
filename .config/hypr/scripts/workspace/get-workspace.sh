f317e65950877e0e9de21ba3e06c6cb97d28bbeb
blendonl <blendonluta@gmail.com>
2026-02-10 11:57:46 +0100

    enhance workspace selector with filtering and custom dispatch

--- .config/hypr/scripts/workspace/get-workspace.sh
+++ .config/hypr/scripts/workspace/get-workspace.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+FILTER_KEYWORD=$1
+DISPATCH=$2
+
+all_workspaces=$(grep -oP '(name|special):[^,]*' ~/.config/hypr/hyprland/hyprland-workspaces.conf)
+
+if [ -n "$FILTER_KEYWORD" ]; then
+  filtered_workspaces=$(echo "$all_workspaces" | grep "$FILTER_KEYWORD")
+else
+  filtered_workspaces="$all_workspaces"
+fi
+
+not_sorted=$(echo "$filtered_workspaces" | sed 's/^name://')
+
+display_names=$(echo "$filtered_workspaces" | sed 's/^name://' | sed 's/^special://' | sed 's/[[:space:]]*$//' | sort)
 
 
-workspace=$(cat ~/.config/hypr/hyprland/hyprland-workspaces.conf  | grep -oP '(name|special):[^,]*' |  wofi --show dmenu)
 
-hyprctl dispatch focusworkspaceoncurrentmonitor $workspace
+selected=$(printf '%s\n' "$display_names" | wofi --show dmenu --sort-order=default)
+[ -z "$selected" ] && exit 0
+
+workspace=$(echo "$filtered_workspaces" | grep "$selected")
+
+hyprctl dispatch $DISPATCH "$workspace"
 
 
 
