c1f63952397410e091e289ab567c40d605fd0cef
blendonl <blendonluta@gmail.com>
2026-02-10 11:58:00 +0100

    update normal submap: add monitor, rename mouse to click, add TAB for workspace move

--- .config/hypr/scripts/submaps_setup/submaps/normal.sh
+++ .config/hypr/scripts/submaps_setup/submaps/normal.sh
@@ -4,14 +4,16 @@ source ~/.config/hypr/scripts/submaps_setup/submap.sh normal bindr=super_l,super
 
 
 add_bind "return" "exec, alacritty" "Open terminal"
+add_bind "m" "submap, monitor" "Monitor"
 add_bind "s" "submap, search" "Search"
-add_bind "m" "submap, mouse" "Mouse"
+add_bind "c" "submap, mouse" "Click"
 add_bind "n" "submap, notes" "Notes"
 add_bind "i" "submap, notification" "Notifications"
 add_bind "p" "submap, power" "Power"
 add_bind "r" "submap, record" "Record"
 add_bind "g" "submap, go" "Go to"
 add_bind "w" "submap, window" "Window management"      
+add_bind "TAB" "movecurrentworkspacetomonitor, +1" "Move Workspace to Monitor"
 add_bind "space" "focusworkspaceoncurrentmonitor, previous" "Go to previous workspace"
 
 
