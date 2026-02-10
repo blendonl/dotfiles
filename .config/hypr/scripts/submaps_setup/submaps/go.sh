e9585d4688113f298bfb37fbe3aa5ddbcd00757f
blendonl <blendonluta@gmail.com>
2026-02-10 11:58:12 +0100

    simplify go submap to use workspace selector for work/personal

--- .config/hypr/scripts/submaps_setup/submaps/go.sh
+++ .config/hypr/scripts/submaps_setup/submaps/go.sh
@@ -2,10 +2,12 @@
 
 source ~/.config/hypr/scripts/submaps_setup/submap.sh go
 
-add_bind "w" "submap, work" "Work"
-add_bind "p" "submap, personal" "Personal"
-add_bind "g" "submap, gaming" "Gaming"
-add_bind "c" "submap, content" "Content"
+# add_bind "w" "submap, work" "Work"
+# add_bind "p" "submap, personal" "Personal"
+# add_bind "g" "submap, gaming" "Gaming"
+# add_bind "c" "submap, content" "Content"
+add_bind "w" "exec, ~/.config/hypr/scripts/workspace/get-workspace.sh work focusworkspaceoncurrentmonitor " "Work"
+add_bind "p" "exec, ~/.config/hypr/scripts/workspace/get-workspace.sh personal focusworkspaceoncurrentmonitor " "Personal"
 
 
 
