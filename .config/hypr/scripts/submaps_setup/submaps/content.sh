56db370d0b3af6a0a47d3cb84c9b4eeb8f14a6a2
blendonl <blendonluta@gmail.com>
2026-02-10 11:58:48 +0100

    update content submap: change key 5 to y for youtube

--- .config/hypr/scripts/submaps_setup/submaps/content.sh
+++ .config/hypr/scripts/submaps_setup/submaps/content.sh
@@ -2,7 +2,7 @@
 
 source ~/.config/hypr/scripts/submaps_setup/submap.sh content
 
-add_bind "5" "focusworkspaceoncurrentmonitor, name:gh" "Go to workspace 5"
+add_bind "y" "focusworkspaceoncurrentmonitor, name:youtube" "Go to Youtube"
 add_bind "m" "focusworkspaceoncurrentmonitor, name:music" "Go to Music"
 
 
